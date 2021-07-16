49daed3262724b554d44d6520b2122ab6b8611c7
Bluedragon137 <alexanderlan137@gmail.com>
2021-07-15 18:04:30 -0700

    *args method test

--- modisco/visualization/viz_sequence.py
+++ modisco/visualization/viz_sequence.py
@@ -128,12 +128,12 @@ def plot_weights_given_ax(ax, array,
     ax.yaxis.label.set_fontsize(15)
 
 
-def plot_weights(filepath, array, figsize=(20,2), **kwargs):
+def plot_weights(array, figsize=(20,2), *args, **kwargs):
     fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111) 
     plot_weights_given_ax(ax=ax, array=array,**kwargs)
     plt.show()
-    plt.savefig(filepath)
+    plt.savefig(args[1])
 
 def plot_score_track_given_ax(arr, ax, threshold=None, **kwargs):
     ax.plot(np.arange(len(arr)), arr, **kwargs)
